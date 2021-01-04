5f685dd99e0486990020633d680e81ef271f28f2
MateuszM99 <mlodochowski.mateusz@gmail.com>
2021-01-05 00:35:48 +0100

    added styling to profile page

--- STalk.Web/Client/src/components/ProfileWindow/ProfileWindow.tsx
+++ STalk.Web/Client/src/components/ProfileWindow/ProfileWindow.tsx
@@ -1,10 +1,10 @@
 import React,{ useEffect, useState } from 'react'
+import './style.scss'
 
 function ProfileWindow() {
     const [image,setImage] = useState<any>("https://thumbs.dreamstime.com/b/default-avatar-profile-image-vector-social-media-user-icon-potrait-182347582.jpg");
     
     const onSelectFile = (event) => {
-        console.log('smthg');
         if (event.target.files && event.target.files[0]) {
           var reader = new FileReader();
         
@@ -20,13 +20,17 @@ function ProfileWindow() {
     }
 
     return (
-        <div>
-            <img className="profile-image" src={image} alt=""/>
-            <input type="file" accept="image/*" onChange={onSelectFile}></input>
-            <button>Change profile image</button>
-            <button>Change password</button>
-            <button>Change email</button>
-            <button>Change username</button>
+        <div className="profile__main col-md-10">
+            <div className="profile__image__change">
+                <img className="profile-image" src={image} alt=""/>
+                <input type="file" accept="image/*" onChange={onSelectFile}></input>
+                <button className="profile__switch__button">Save profile image</button>
+            </div>
+            <div className="profile__form__control">
+            <button className="profile__switch__button">Change password</button>
+            <button className="profile__switch__button">Change email</button>
+            <button className="profile__switch__button">Change username</button>
+            </div>
         </div>
     )
 }
